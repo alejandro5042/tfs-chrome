d12e8702643a631256e6a92b8412fda7c7fc71fc
Alejandro Barreto <alejandro5042@gmail.com>
2013-12-13 14:59:13 -0600

    Search for changesets and shelvesets
    Includes nicer options page and icons. Automatically takes you to the
    options if baseUrl not set. Smart searching based on what you type so
    you don't need to type changeset or shelveset in front.

--- background.js
+++ background.js
@@ -1,23 +1,64 @@
-chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
-    suggest([
-      {content: text + " one", description: "the first one"},
-      {content: text + " number two", description: "the second entry"}
-    ]);
-});
 
-function navigate(url) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.update(tabs[0].id, {url: url});
-  });
+// chrome.omnibox.setDefaultSuggestion({ description: "<url>tf</url> <dim>[</dim><url>shelveset</url> #<dim>]</dim>" })
+// chrome.omnibox.setDefaultSuggestion({ description: "awesome" });
+
+searches =
+    [
+        {
+            prefix: "changeset ",
+            description: "displays a changeset",
+            pattern: /^(?:changeset\s+)?(C?\d+)/i,
+            transform: "_versionControl/changeset/$1"
+        },
+        {
+            prefix: "shelveset ;",
+            description: "browse a user's shelveset",
+            pattern: /^(?:shelveset\s+)?;(.*)/i,
+            transform: "_versionControl/shelvesets#owner=$1"
+        },
+        {
+            prefix: "shelveset ",
+            description: "displays a shelveset",
+            pattern: /^(?:shelveset\s+)?(.*)/i,
+            transform: "_versionControl/shelveset/?ss=$1"
+        }
+    ];
+    
+chrome.omnibox.onInputChanged.addListener(function(text, suggest)
+    {
+        suggest(searches.map(function(search)
+            {
+                return {
+                    content: search.prefix + text,
+                    description: "<match>" + search.prefix + "</match>" + text + "<dim> - " + search.description + "</dim>"
+                }
+            }));
+    });
+
+function navigate(url)
+{
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.update(tabs[0].id, {url: url});
+    });
 }
 
-chrome.omnibox.onInputEntered.addListener(function(text) {
-    var baseUrl = localStorage["baseUrl"];
-    if (!baseUrl)
+chrome.omnibox.onInputEntered.addListener(function(text)
     {
-        alert("Set base URL in extension options!");
-        return;
-    }
+        if (!localStorage["baseUrl"])
+        {
+            navigate("options.html");
+            return;
+        }
 
-    navigate(baseUrl + "_versionControl/changeset/" + text);
-});
+        for (var i in searches)
+        {
+            var url = text.replace(searches[i].pattern, localStorage["baseUrl"] + searches[i].transform);
+            if (url != text)
+            {
+                navigate(url);
+                return;
+            }
+        }
+        
+        navigate(localStorage["baseUrl"]);
+    });
--- options.js
+++ options.js
@@ -1,22 +1,66 @@
-// Saves options to localStorage.
-function save_options() {
-  localStorage["baseUrl"] = document.getElementById("baseUrl").value;
+$(document).ready(function() {
 
-  // Update status to let user know options were saved.
-  var status = document.getElementById("status");
-  status.innerHTML = "Options Saved.";
-  setTimeout(function() {
-    status.innerHTML = "";
-  }, 750);
-}
+    var statusTimer = null;
+    
+    function flash(text, color)
+    {
+        $("#status").css('color', color);
+        $("#status").html(text);
+        
+        clearTimeout(statusTimer);
+        statusTimer = setTimeout(function() { $("#status").html(""); }, 750);
+    }
 
-// Restores select box state to saved value from localStorage.
-function restore_options() {
-  var baseUrl = localStorage["baseUrl"];
-  if (!favorite) {
-    return;
-  }
-  document.getElementById("baseUrl").value = favorite;
-}
-document.addEventListener('DOMContentLoaded', restore_options);
-document.querySelector('#save').addEventListener('click', save_options);
+    function save_options()
+    {
+        url = $("#baseUrl").val();
+        
+        // Add a trailing slash.
+        url = url.replace(/\/?$/, '/');
+        
+        if (validateURL(url))
+        {
+            localStorage["baseUrl"] = url;
+            flash("Options saved!", "#080");
+            
+            // Make sure we reload it to ensure the user sees what he actually saved.
+            restore_options();
+        }
+        else
+        {
+            flash("Invalid URL", "#800");
+        }
+    }
+
+    function restore_options() {
+        $("#baseUrl").val(localStorage["baseUrl"]);
+    }
+    
+    function validateURL(url)
+    {
+        var validator = new RegExp( "^((http|https|ftp)\://)?([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$");
+        return validator.test(url);
+    }
+    
+    function setup_page()
+    {
+        var manifest = chrome.runtime.getManifest();
+        document.title = manifest.name + " Options";
+        $("#name").html(manifest.name + " " + manifest.version);
+        $("#description").html(manifest.description);
+        $("#logo").attr('src', manifest.icons['128']);
+        $("#baseUrl").focus();
+    }
+    
+    function hook_events()
+    {
+        $("#config-form").submit(function(e) {
+            save_options();
+            e.preventDefault();
+        });
+    }
+
+    restore_options();
+    setup_page();
+    hook_events();
+});
